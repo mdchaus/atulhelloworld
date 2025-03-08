4d8c095bc8e8c1ab921f7e044512840546262938
mdchaus <mohd.chaus@gmail.com>
2025-03-09 00:42:55 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,6 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/basic-java-app-1.0-SNAPSHOT.jar
+java -jar target/basic-java-app-1.0-SNAPSHOT.jar --server.port=8081
+
+
